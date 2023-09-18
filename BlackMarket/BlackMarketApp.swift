0b9e0ed400f81c98595a185dc4f13dfcd6a26af0
Santiago Andracchi <santiago.andracchi@gmail.com>
2023-09-18 14:13:32 -0300

    Initial Commit

---
+++ BlackMarket/BlackMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackMarketApp.swift
+//  BlackMarket
+//
+//  Created by Santiago Andracchi on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlackMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
